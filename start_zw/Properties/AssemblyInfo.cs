7f8b05c2e3bd5f96e5652224e227070a644a9ef1
WaGaCAD <waldek@parikon.pl>
2018-12-03 14:02:34 +0100

    20181203

--- start_zw/Properties/AssemblyInfo.cs
+++ start_zw/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("start_zw")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 WaGaCAD")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ start_zw/Start.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using zzr = ZwSoft.ZwCAD.Runtime;
+using zza = ZwSoft.ZwCAD.ApplicationServices;
+using zze = ZwSoft.ZwCAD.EditorInput;
+
+namespace start_zw
+{
+    public class Start : zzr.IExtensionApplication
+    {
+        string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        int zlicz = 0;
+        public static zza.Document doc = zza.Application.DocumentManager.MdiActiveDocument;
+        zze.Editor ed = doc.Editor;
+
+        public void Initialize()
+        {
+            Zaladuj("skala_zw.dll");
+
+            if (zlicz == 0)
+            {               
+                ed.WriteMessage("\nPI 2019 MIT Edition załadowano poprawnie. Niektóre moduły PI korzystają z silnika bazy danych SQLite - www.sqlite.org.");
+            }
+            else
+            {                
+                ed.WriteMessage("\nPI 2019 MIT Edition załadowano z błędami. Informacje o błędach mogą się znajdować we wcześniejszych komunikatach");
+            }
+        }
+        public void Terminate()
+        {
+            
+        }
+
+        public void Zaladuj(string nazwadll)
+        {            
+            string sciezka = path + "\\" + nazwadll;            
+            try
+            {
+                Assembly.LoadFrom(sciezka);
+            }
+            catch (System.Exception ex)
+            {
+                ed.WriteMessage(ex.Message);
+                zlicz = zlicz = 1;
+            }
+        }
+    }
+}
+
